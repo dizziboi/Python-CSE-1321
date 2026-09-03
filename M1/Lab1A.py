7a086fc053f6d1fe0f9a6b1b5c626d47665d3840
caleb-b <c.a.brown0914@gmail.com>
2026-09-03 11:49:27 -0400

    Updated folder stucture and uploaded lab1 assignments

---
+++ M1/Lab1A.py
@@ -0,0 +1,13 @@
+# Class: CSE 1321L
+# Section: B09
+# Term: Fall
+# Instructor: Jui Mhatre
+# Name: Caleb Brown
+# Lab: 1
+print ("Three... ", end =" ")
+print ("Two... ", end =" ")
+print ("One... ", end =" ")
+print ("Zero..." , end =" ")
+print ("Liftoff!")
+# appears on first line
+print ("Houston, we have a problem.")
---
+++ M1/Lab1B.py
@@ -0,0 +1,8 @@
+# Class: CSE 1321L
+# Section: B09
+# Term: Fall
+# Instructor: Jui Mhatre
+# Name: Caleb Brown
+# Lab: 1
+message = input ("Enter a line of text: ");
+print ("You wrote '" + message + "'");
