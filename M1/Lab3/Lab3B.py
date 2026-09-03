ecc8c67a8cb999b21dc7c83a8d84dd54bebbde51
caleb-b <c.a.brown0914@gmail.com>
2026-09-03 14:52:25 -0400

    Finished Lab3B

--- M1/Lab3/Lab3B.py
+++ M1/Lab3/Lab3B.py
@@ -6,16 +6,20 @@
 # Lab: 1
 C1H = int(input("Course 1 hours: "))
 C1G = int(input("Grade for course 1:"))
+qualityPoint1=C1H*C1G
 C2H = int(input("Course 2 hours: "))
 C2G = int(input("Grade for course 2:"))
+qualityPoint2=(C2H*C2G)
 C3H = int(input("Course 3 hours: "))
 C3G = int(input("Grade for course 3:"))
+qualityPoint3=(C3H*C3G)
 C4H = int(input("Course 4 hours: "))
 C4G = int(input("Grade for course 4:"))
+qualityPoint4=(C4H*C4G)
 totalHours = C1H + C2H + C3H + C4H
-totalQualityPoints = (C1H*C1G) + (C2G+C2H) + (C1H*C1G) + (C2G+C2H)
+totalQualityPoints = qualityPoint1+qualityPoint2+qualityPoint3+qualityPoint4
 GPA = totalQualityPoints/totalHours
 
-print(totalHours)
-print(totalQualityPoints)
-print(GPA)
+print("Total hours:",totalHours)
+print("Total quality points",totalQualityPoints)
+print("Your GPA for this semester is ",round(GPA,2))
