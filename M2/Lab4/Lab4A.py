b75cd9aa6b99eebe2633aabf6988eecb04fe6e5c
Caleb_B <dizziboi07@gmail.com>
2026-09-10 12:48:24 -0400

    Started work on Lab4a

---
+++ M2/Lab4/Lab4A.py
@@ -0,0 +1,16 @@
+# Class: CSE 1321L
+# Section: B09
+# Term: Fall
+# Instructor: Jui Mhatre
+# Name: Caleb Brown
+# Lab: 1
+grade = float(input("Enter your grade:"))
+
+if grade >= 97 <=100:
+    print("A+")
+elif grade <=96 >=94:
+    print("A")
+elif grade >=91 <=93:
+    print("A-")
+elif grade <=88 >=90:
+    print("B+")
