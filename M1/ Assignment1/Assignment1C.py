fe973cc21de472a66816fa4bb7d9b2a5c465dfb6
Caleb_B <dizziboi07@gmail.com>
2026-09-09 20:14:41 -0400

    Finished Assignment1C

--- M1/ Assignment1/Assignment1C.py
+++ M1/ Assignment1/Assignment1C.py
@@ -4,6 +4,13 @@
 # Instructor: Jui Mhatre
 # Name: Caleb Brown
 # Lab: 1
-sampleRate = int(input("Enter Sample rate(in Hz)"))
-bufferSize = int(input("Enter Sample rate(in Hz)"))
-trackLength = int(input("Enter Sample rate(in Hz)"))
+sampleRate = int(input("Enter Sample Rate (in Hz): "))
+bufferSize = int(input("Enter Buffer Size: "))
+trackLength = int(input("Enter track length (in seconds): "))
+
+step1 = sampleRate*trackLength #Multiples the sample rate by the track length
+step2 = step1 // bufferSize #Uses floor to find the full buffer
+#print(step2)
+step3 = step1 % bufferSize #Uses mod to find the leftover samples
+#print(step3)
+print("Processed",step2,"full buffers with",step3,"leftover samples.")
